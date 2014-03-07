fe2283f31e434cec54ca1bb2488915f14abdd6b8
Kevin Roberts <kroberts@citytechinc.com>
2014-03-07 13:48:12 -0600

    added stock domain for typeahead data

---
+++ grails-app/domain/stockticker/Stock.groovy
@@ -0,0 +1,23 @@
+package stockticker
+
+class Stock {
+
+	String name
+	String symbol
+	Date created
+	Date lastUpdated
+
+	def beforeInsert() {
+		created = new Date()
+		lastUpdated = new Date()
+	}
+
+	def beforeUpdate() {
+		lastUpdated = new Date()
+	}
+
+    static constraints = {
+		symbol(nullable: false, blank: false, unique: true)
+		name(nullable: true)
+    }
+}
